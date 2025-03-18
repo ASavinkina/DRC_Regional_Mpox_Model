d3ed70a686c904d608d352ad6921dbd23bf970cc
Alexandra Savinkina <alexandra.savinkina@yale.edu>
2025-03-18 15:11:23 -0400

    Add births and deaths data to demographic data

--- Population_Data_Countries.R
+++ Population_Data_Countries.R
@@ -14,6 +14,8 @@ Countries <- unique(demo_data$Country)
 # 
 Countries <- Countries[!Countries %in% c("DRC")]
 
+demo_data$birth_per_1000 <- 0
+
 for (i in 1:length(Countries)) {
   
   i=Countries[i]
@@ -24,13 +26,13 @@ data_pop <- data %>% filter(grepl(c('Population'), Indicator.Name))
 
 data_mort <- data %>% filter(grepl(c('Mortality'), Indicator.Name))
 
-data_birth <- data %>% filter(grepl(c('Birth'), Indicator.Name))
+data_birth <- data %>% filter(grepl(c('Birth rate, crude'), Indicator.Name))
 
 data_pop2 <- data_pop %>% filter(Indicator.Name %in% c("Population, male", "Population, female", "Population ages 15-64, female",
                                         "Population ages 15-64, male","Population ages 0-14, female",
                                         "Population ages 0-14, male", "Population ages 00-04, male (% of male population)",
                                         "Population ages 00-04, female (% of female population)", "Population ages 65 and above, male",
-                                        "Population ages 65 and above, female")) 
+                                        "Population ages 65 and above, female", "")) 
 
 data_pop2 <- data_pop2 %>% select(Country.Name, Indicator.Name,X2023)
 
@@ -50,11 +52,14 @@ sum(data_pop2_wide$`Population, female`, data_pop2_wide$`Population, male`)
 sum(data_pop2_wide$U5, data_pop2_wide$O15, data_pop2_wide$U15)
 
 
-
 demo_data$U5 <- ifelse(demo_data$Country==i, data_pop2_wide$U5,demo_data$U5 )
 demo_data$U15 <- ifelse(demo_data$Country==i, data_pop2_wide$U15,demo_data$U15 )
 demo_data$O15 <- ifelse(demo_data$Country==i, data_pop2_wide$O15,demo_data$O15 )
 
+demo_data$birth_per_1000 <- ifelse(demo_data$Country==i, data_birth$X2022, demo_data$birth_per_1000)
+
 }
 
-write.csv(demo_data, file="Data/Demographic_data_countries_2_27_25.csv")
+demo_data$birth_per_1000 <- ifelse(demo_data$Country=="DRC", 40.08, demo_data$birth_per_1000)
+
+write.csv(demo_data, file="Data/Demographic_data_countries_3_18_25.csv")
